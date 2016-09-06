b1070f4135de8019abea8c6e4361ca02a26dfa06
Kevin McPhillips <github@kevinmcphillips.ca>
2016-09-05 20:52:56 -0400

    Add ruby and rails env

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -2,6 +2,8 @@ class PingController < ApplicationController
 	def index
     @tests = {
       mysql: !!ActiveRecord::Base.connection.execute("select 1"),
+      environment: Rails.env,
+      ruby: RUBY_VERSION,
       internet: "definitely",
     }.with_indifferent_access
   end
