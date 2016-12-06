8d149f44f302114226fb85a7bc825a9e9098ba49
Kevin McPhillips <github@kevinmcphillips.ca>
2016-12-05 22:57:18 -0500

    Revert "Add flag to show reload"
    This reverts commit de3d08cafa33467badf2e08460b47f8ac5450032.

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -6,7 +6,6 @@ class PingController < ApplicationController
       ruby: RUBY_VERSION,
       internet: "definitely",
       path: Rails.root.split.last.to_s,
-      reloaded: "yes it did reload thank you",
     }.with_indifferent_access
   end
 end
