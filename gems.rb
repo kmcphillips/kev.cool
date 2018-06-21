d90b7ac9542770f75687ea97b3727ecef552645f
Kevin McPhillips <github@kevinmcphillips.ca>
2018-06-20 20:41:37 -0400

    Update to Rails 5.2.0

--- gems.rb
+++ gems.rb
@@ -1,8 +1,9 @@
 source 'https://rubygems.org'
 
-gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
+gem 'rails', '5.2.0'
 gem 'mysql2'
 gem 'puma', '~> 3.0'
+gem 'bootsnap'
 
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
