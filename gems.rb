e31527d9cc4e8385ce8707aed70d867f0ac7c2bc
Kevin McPhillips <github@kevinmcphillips.ca>
2018-06-20 20:34:10 -0400

    Remove coffee

--- gems.rb
+++ gems.rb
@@ -6,8 +6,6 @@ gem 'puma', '~> 3.0'
 
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
-gem 'coffee-rails', '~> 4.2'
-# gem 'therubyracer', platforms: :ruby
 gem 'jquery-rails'
 
 group :development, :test do
