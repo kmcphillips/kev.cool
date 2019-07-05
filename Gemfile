3c6aa7001ac9411f9bb4fb0df8aa8cb131513686
Kevin McPhillips <github@kevinmcphillips.ca>
2019-07-04 23:29:36 -0400

    Add sidekiq web monitoring

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '~> 5.2.0'
 gem 'mysql2'
 gem 'puma'
 gem 'bootsnap'
+gem 'sidekiq'
 
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root to: "ping#index"
+
+  require "sidekiq/web"
+  Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(Rails.application.secrets[:sidekiq_admin_username])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(Rails.application.secrets[:sidekiq_admin_password]))
+  end
+  mount Sidekiq::Web, at: "/sidekiq"
 end
