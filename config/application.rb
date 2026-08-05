e9ce7643bd12d1008cdfcf9eb4f12b5eefdad665
Kevin McPhillips <github@kevinmcphillips.ca>
2026-08-05 10:43:52 -0400

    Fix deprecations and vips explicit variant processor

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,8 @@ module KevCool
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.active_support.to_time_preserves_timezone = :zone
+    # Disable Active Storage variant processing, but could use vips or mini_magick instead
+    config.active_storage.variant_processor = :disabled
 
     # Don't generate system test files.
     config.generators.system_tests = nil
